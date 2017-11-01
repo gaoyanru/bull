fa1f12e9c3796b2cda4a285242e8b0cb059e41f0
fangbao <fangbao@wanglibank.com>
2017-11-01 16:15:29 +0800

    修改图片上传

--- app/scripts/controllers/addorder2.js
+++ app/scripts/controllers/addorder2.js
@@ -1,8 +1,17 @@
 'use strict';
 angular.module('channelApp').controller('AddOrderCtrl2', ['$scope', '$http', '$filter', '$state', '$stateParams', 'FileUploader', 'user', function ($scope, $http, $filter, $state, $stateParams, FileUploader, user) {
   $scope.amount = "8047.00"
-  $scope.xxx = function(){
-    alert('xxx');
+  $scope.imgSrc1 = 'https://pilipa.oss-cn-beijing.aliyuncs.com/imgReceipt/63/4707/2017-10-31/EZBe3CmjkH.jpg';
+  $scope.imgSrc2 = '';
+
+  $scope.startDateOptions = {
+      formatYear: 'yyyy'
+  };
+  $scope.endDateOptions = {
+      formatYear: 'yyyy'
+  };
+
+  $scope.save = function(){
+    console.log($scope)
   }
-  $scope.imgSrc1 = '';
 }]);
