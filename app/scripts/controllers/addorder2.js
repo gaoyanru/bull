ffd0d49bfb15179709d453db22e92d58a01c5eb2
gaoyanru <962290336@qq.com>
2017-11-09 10:37:07 +0800

    1.5提单

--- app/scripts/controllers/addorder2.js
+++ app/scripts/controllers/addorder2.js
@@ -1,7 +1,7 @@
 'use strict';
 angular.module('channelApp').controller('AddOrderCtrl2', ['$scope', '$http', '$filter', '$state', '$stateParams', 'FileUploader', 'user', function ($scope, $http, $filter, $state, $stateParams, FileUploader, user) {
   $scope.amount = "8047.00"
-  $scope.imgSrc1 = 'https://pilipa.oss-cn-beijing.aliyuncs.com/imgReceipt/63/4707/2017-10-31/EZBe3CmjkH.jpg';
+  $scope.imgSrc1 = '';
   $scope.imgSrc2 = '';
 
   $scope.startDateOptions = {
