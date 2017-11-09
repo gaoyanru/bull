18c7aa276010e8e236534ba8b7990b854cf4b947
gaoyanru <962290336@qq.com>
2017-11-09 14:22:03 +0800

    1.5版本

--- app/scripts/controllers/addorder2.js
+++ app/scripts/controllers/addorder2.js
@@ -1,6 +1,32 @@
 'use strict';
 angular.module('channelApp').controller('AddOrderCtrl2', ['$scope', '$http', '$filter', '$state', '$stateParams', 'FileUploader', 'user', function ($scope, $http, $filter, $state, $stateParams, FileUploader, user) {
-  $scope.amount = "8047.00"
+  // 获取代理商账户余额
+  function getBanlance() {
+    $http.get('api/agent/balance').success(function (result) {
+      $scope.balance = result.data;
+    })
+  }
+  getBanlance()
+  // 表单提交数据
+  $scope.postData = {}
+  // 初始化获取接口数据 销售
+  function initDict() {
+    $http.get("/api/orders/sales").success(function (data) {
+      $scope.sales = data.data;
+    })
+  }
+  initDict()
+  // 控制是否预提单 category==1新增 category==2预提单
+  $scope.category = 1
+  $scope.setCategory = function () {
+    if ($scope.postData.OrderId) return
+    if ($scope.isNewCompany) {
+      $scope.category = 2
+    } else {
+      $scope.category = 1
+    }
+  }
+
   $scope.imgSrc1 = '';
   $scope.imgSrc2 = '';
 
